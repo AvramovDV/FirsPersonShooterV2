64c84c2ad0eb34ca877c98cdf3ba625a1d668f16
AvramovDenis <avramov.91@mail.ru>
2020-07-20 18:42:06 +0300

    Добавил PathModel и PathInspector для создания и редактирования путей для ботов в Editor

---
+++ FirstPersonShooterV2/Assets/Editor/PathInspector.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(PathModel))]
+public class PathInspector : Editor
+{
+    private void OnSceneGUI()
+    {
+        PathModel pathModel = target as PathModel;
+
+        Vector3[] points = pathModel.GetPoints();
+
+        Handles.color = Color.red;
+
+        Handles.DrawAAPolyLine(10f, points);
+
+        for (int i = 0; i < (pathModel.Loop ? points.Length - 1 : points.Length); i++)
+        {
+            EditorGUI.BeginChangeCheck();
+            points[i] = Handles.DoPositionHandle(points[i], Quaternion.identity);
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(pathModel, "Move Point");
+                EditorUtility.SetDirty(pathModel);
+                pathModel.Points[i].position = points[i];
+            }
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        PathModel pathModel = target as PathModel;
+        if (GUILayout.Button("Add Point"))
+        {
+            pathModel.AddPoint();
+        }
+    }
+}
---
+++ FirstPersonShooterV2/Assets/Scripts/Models/PathModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class PathModel : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private bool _loop;
+    private List<Transform> _points = new List<Transform>();
+
+    #endregion
+
+
+    #region Propeties
+
+    public List<Transform> Points { get => _points; }
+    public bool Loop { get => _loop; }
+
+    #endregion
+
+
+    #region Methods
+
+    public Vector3[] GetPoints()
+    {
+        _points = new List<Transform>();
+        _points = transform.GetComponentsInChildren<Transform>().ToList<Transform>();
+        _points.Remove(transform);
+
+        if (_loop)
+        {
+            _points.Add(_points[0]);
+        }
+
+        return (from a in _points select a.position).ToArray<Vector3>();
+    }
+
+    public void AddPoint()
+    {
+        Transform newPoint = new GameObject($"PathPoint({_points.Count})").transform;
+        if (_points.Count > 1)
+        {
+            newPoint.position = Vector3.Lerp(_points[0].position, _points[(_loop ? _points.Count - 2 : _points.Count - 1)].position, 0.5f);
+        }
+        else
+        {
+            newPoint.position += Vector3.forward * 5f;
+        }
+        newPoint.parent = transform;
+        _points.Add(newPoint);
+
+    }
+
+    #endregion
+}
