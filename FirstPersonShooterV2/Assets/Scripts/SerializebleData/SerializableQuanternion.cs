0d7e016660d0f29f5a2a721840ed8f48c2d4ac2a
AvramovDenis <avramov.91@mail.ru>
2020-07-23 17:24:26 +0300

    Добавил сохранение и загрузку данных о положении игрока с помощью BinaryFormatter (сделал как было на уроке)

---
+++ FirstPersonShooterV2/Assets/Scripts/SerializebleData/SerializableQuanternion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SerializableQuanternion
+{
+    #region Fields
+
+    public float X;
+    public float Y;
+    public float Z;
+    public float W;
+
+    #endregion
+
+
+    #region ClassLifeCycles
+
+    public SerializableQuanternion(float x, float y, float z, float w)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+        W = w;
+    }
+
+    #endregion
+
+
+    #region Methods
+
+    public static implicit operator Quaternion(SerializableQuanternion value)
+    {
+        return new Quaternion(value.X, value.Y, value.Z, value.W);
+    }
+
+    public static implicit operator SerializableQuanternion(Quaternion value)
+    {
+        return new SerializableQuanternion(value.x, value.y, value.z, value.w);
+    }
+
+    #endregion
+}
